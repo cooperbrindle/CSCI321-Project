bcdab6d2f929ea46a040f595888afbb8e205fb0d
Shiny380 <daniel.mckinnell43@gmail.com>
2018-04-27 20:32:10 +1000

    Added start of FeedPage and edited LoginPage

---
+++ Components/FeedComps/FeedPage.js
@@ -0,0 +1,22 @@
+
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, TextInput, Button, View} from 'react-native'; 
+
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#0C2340',
+    },
+    
+});
--- Components/LoginComps/LoginPage.js
+++ Components/LoginComps/LoginPage.js
@@ -1,59 +1,74 @@
 
 
 import React, { Component } from 'react';
-import {Platform, StyleSheet, Text, TextInput, Button, View} from 'react-native'; 
+import { Platform, StyleSheet, Text, TextInput, Button, View} from 'react-native'; 
 
-type Props = {} ;
 
-export default class App extends Component<Props> {
+export default class Login extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.Title}>
-          UOW Alumni
-        </Text>
-        <TextInput style={styles.inputBox}
-            placeholder="student number"/>
-        <TextInput style={styles.inputBox}
-            placeholder="password"/>
-        <Button style={styles.loginBtn}
-            title="Login"
-            onPress={}/>
-        <Button style={styles.socialBtn}
-            title="Google"
-            onPress={}/>
-        <Button style={styles.socialBtn}
-            title="Facebook"
-            onPress={}/>
-        <Button style={styles.socialBtn}
-            title="LinkedIn"
-            onPress={}/>
+        <view style={styles.titleContainer}>
+            <Text style={styles.Title}>
+                UOW Alumni
+            </Text>
+        </view>
+        <view style={styles.inputContainer}>
+            <TextInput style={styles.inputBox}
+                placeholder="student number"/>
+            <TextInput style={styles.inputBox}
+                placeholder="password"/>
+            <Button style={styles.loginBtn}
+                title="Login"
+                onPress={{}}/>
+        </view>
+        <view style={styles.socialContainer}>
+            <Button style={styles.socialBtn}
+                title="Google"
+                onPress={{}}/>
+            <Button style={styles.socialBtn}
+                title="Facebook"
+                onPress={{}}/>
+            <Button style={styles.socialBtn}
+                title="LinkedIn"
+                onPress={{}}/>
+        </view>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  Title: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-  },
-  inputBox: {
-    textAlign: 'left',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  loginBtn: {
-
-  },
-  socialBtn: {
-
-  }
+    container: {
+        backgroundColor: '#0C2340',
+    },
+    titleContainer: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        flex: 2,
+    },
+    socialContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    Title: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+        color: 'white',
+        /*fontStyle: 'Chronicle Display Roman'*/
+    },
+    inputBox: {
+        textAlign: 'left',
+        marginBottom: 5,
+    },
+    loginBtn: {
+        marginBottom: 5,
+    },
+    socialBtn: {
+        flex: 1,
+    }
 });
