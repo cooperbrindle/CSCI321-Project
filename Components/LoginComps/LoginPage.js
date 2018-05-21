5ae8e32013e4daa4facbb26b3c73f0eb2e8a610d
unknown <daniel.mckinnell43@gmail.com>
2018-05-22 00:59:05 +1000

    near finished loginPage

--- Components/LoginComps/LoginPage.js
+++ Components/LoginComps/LoginPage.js
@@ -15,27 +15,30 @@ export default class Login extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View  style={styles.titleContainer}>
+
+
+        <View style={styles.logoCont}>
             <Image
-                style={{width: 380, height: 120, paddingBottom: 10,}}
+                style={styles.logo}
+                resizeMode='center'
+                resizeMethod='resize'
                 source={require('./logo.png')}
             />
+            <View style={styles.banner}>
+                    <Text style={styles.bannerText}>
+                        ALUMNI
+                    </Text>
+            </View>
         </View>
-        <View style={styles.banner}>
-                <Text>
-                    Alumni
-                </Text>
-        </View>
-
 
         <View style={styles.inputContainer}>
             <TextInput style={styles.inputBox}
-                placeholder="student number" underlineColorAndroid='transparent' placeholderTextColor='#cc0000'/>
+                placeholder="student number" underlineColorAndroid='transparent' placeholderTextColor='grey'/>
             <TextInput style={styles.inputBox}
-                placeholder="password" underlineColorAndroid='transparent' placeholderTextColor='#cc0000'/>
+                placeholder="password" underlineColorAndroid='transparent' placeholderTextColor='grey'/>
             <TouchableHighlight style={styles.loginBtn}
                 onPress={() => this.props.navigation.navigate('Feed')}>
-                <Text style={styles.socialText}>
+                <Text style={styles.loginText}>
                         LOGIN
                 </Text>
             </TouchableHighlight>
@@ -44,13 +47,13 @@ export default class Login extends React.Component {
 
 
         <View style={styles.socialContainer}>
-            <TouchableHighlight style={styles.socialBtn}
+            <TouchableHighlight style={styles.socialBtnFB}
                 onPress={this._onPressButton}>
                     <Text style={styles.socialText}>
                         Continue with facebook
                     </Text>
             </TouchableHighlight>
-            <TouchableHighlight style={styles.socialBtn}
+            <TouchableHighlight style={styles.socialBtnLI}
                 onPress={this._onPressButton}>
                     <Text style={styles.socialText}>
                         Continue with Linkedin
@@ -66,79 +69,89 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0C2340',
+    },
 
+    logoCont: {
+        flex: 2,
     },
     logo: {
-        flex: 1,
-        width: 300,
-        height: 250,
+        width: 252,
+        height: 101,
+        flexDirection: 'row',
+        alignSelf: 'center',
     },
     banner: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        marginLeft: 0,
-        marginRight: 0,
+        backgroundColor: 'white',
+        margin: 0,
         justifyContent: 'center',
-        alignItems: 'center',
+        padding: 10,
         flexDirection: 'row',
-        marginTop:5,
     },
-    titleContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row'
+    bannerText: {
+        fontSize: 24,
+        color: '#0C2340',
+        fontWeight: 'bold',
     },
+
+
+    /////////////////////////////////////////INPUT STYLES
     inputContainer: {
-        flex: 1,
-        marginLeft: 40,
-        marginRight: 40,
-        marginBottom: 20,
+        flex: 2,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 40,
+        marginBottom: 60,
     },
     inputBox: {
-        flex: 1,
-        marginRight:20,
         alignItems: 'center',
-        marginLeft:20,
-        color: '#cc0000',
-        marginTop:5,
-        paddingTop:5,
-        paddingBottom:5,
+        color: 'grey',
+        marginBottom: 0,
+        marginTop: 0,
+        paddingLeft: 10,
         backgroundColor:'#d9d9d6',
+        height: 45,
         borderRadius:10,
         borderWidth: 1,
-        textAlign: 'center',
     },
-
     loginBtn: {
-        flex: 1,
-        marginRight:20,
+        justifyContent: 'center',
         alignItems: 'center',
-        marginLeft:20,
         marginTop:5,
-        paddingTop:5,
-        paddingBottom:5,
-        backgroundColor:'#cc0000',
+        backgroundColor:'#0C2340',
+        height: 45,
         borderRadius:10,
-        borderWidth: 1,
+        borderWidth: 2,
+        borderColor: '#d9d9d6',
+    },
+    loginText: {
+        color: '#cc0000',
     },
 
+    /////////////////////////////////////////SOCIAL STYLES
     socialContainer: {
         flex: 1,
-        flexDirection: 'column',
-        paddingBottom: 20,
+        marginBottom: 30,
+        marginLeft: 5,
+        marginRight: 5,
     },
-    socialBtn: {
-        flex: 1,
-        marginRight:20,
+    socialBtnFB: {
         alignItems: 'center',
-        marginLeft:20,
-        marginTop:5,
-        paddingTop:5,
-        paddingBottom:5,
-        backgroundColor:'#cc0000',
+        height: 45,
+        backgroundColor:'#3B5998',
         borderRadius:10,
         borderWidth: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
     },
+    socialBtnLI: {
+        alignItems: 'center',
+        height: 45,
+        backgroundColor:'#0077B5',
+        borderRadius:10,
+        borderWidth: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+    },/*
     socialView: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -147,9 +160,9 @@ const styles = StyleSheet.create({
     socialImage: {
         width: 20,
         height: 20,
-    },
+    },*/
     socialText: {
-        color: '#ffffff',
-        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
     },
 });
