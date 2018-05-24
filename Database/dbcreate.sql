066e962dad86957a2492a5d26971c6df4a59e916
csit321mf06a <csit321mf06a@proj-mac8.adeis.uow.edu.au>
2018-05-24 14:16:15 +1000

    d update

--- Database/dbcreate.sql
+++ Database/dbcreate.sql
@@ -1,51 +1,79 @@
-/* Create Database Tables
-    Design Based on RE Database
+/* Create Database Tables */
+/*    Design Based on RE Database */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*    Last Edited: Cooper Brindle 24/05/2018 12:14pm*/
 
-    Last Edited: Cooper Brindle 14/05/2018 12:14pm
-*/
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* Constituent Table */
 
 /* Cooper 14/05/18 */
 CREATE TABLE CONSTITUENT(
-    constituentid   VARCHAR(100)    NOT NULL, /* Universal Unique Identifier */ /* check storage method */
-    keyname         VARCHAR(100)	NOT NULL, /* Constituent Surname */
-    firstname       VARCHAR(50)     NOT NULL, /* Constituent First Name */
-    middlename      VARCHAR(50)     NULL, /* Constituent Middlename */
-    maidenname      VARCHAR(100)    NULL, /* Constituent Maidenname */
-    nickname        VARCHAR(50)     NULL, /* Preferred name */
+    constituentID   VARCHAR(100)    NOT NULL, /* Universal Unique Identifier */ /* check storage method */
+    keyName         VARCHAR(100)	NOT NULL, /* Constituent Surname */
+    firstName       VARCHAR(50)     NOT NULL, /* Constituent First Name */
+    middleName      VARCHAR(50)     NULL, /* Constituent Middlename */
+    maidenName      VARCHAR(100)    NULL, /* Constituent Maidenname */
+    nickName        VARCHAR(50)     NULL, /* Preferred name */
     ssn             DECIMAL(7)      NOT NULL, /* Student Number */ 
     /*SSNIndex*/
     /*Title Code ID*/
     /*Suffix Code ID*/
     /*Gendercode*/
-    birthdate       VARCHAR(8)      NOT NULL, /* Constituent Birth Date ie 27101996 */
-    isinactive      BINARY(1)       NOT NULL, /* Are the an active Alumni Member */ /* Get active definition */
-    givesanonymously BINARY(1)      NOT NULL, /* Donor Status */
-    webaddress      VARCHAR(2047)   NULL, /* Constituent Web Address */
+    birthDate       VARCHAR(8)      NOT NULL, /* Constituent Birth Date ie 27101996 */
+    isInactive      BINARY(1)       NOT NULL, /* Are the an active Alumni Member */ /* Get active definition */
+    givesAnonymously BINARY(1)      NOT NULL, /* Donor Status */
+    webAddress      VARCHAR(2047)   NULL, /* Constituent Web Address */
     /*Picture*/
     /*Picture Thumbnail*/
-    isorganisation  BINARY(1)       NOT NULL, /* Is this entity an Organisation */
+    isOrganisation  BINARY(1)       NOT NULL, /* Is this entity an Organisation */
     /*Marital Status*/
-    netcommunitymember  BINARY(1)   NOT NULL, /* Net Community Member Status */
-    donotmail       BINARY(1)       NOT NULL, /* Recieve mail notifications */
-    donotemail       BINARY(1)       NOT NULL, /* Recieve email notifications */
-    donotphone       BINARY(1)       NOT NULL, /* Recieve phone calls */
-    customidentifier VARCHAR(100)   NULL,   /* Check current use */
-    addedby         VARCHAR(100)    NOT NULL, /* ID of user who added constituent */
-    changedby       VARCHAR(100)    NULL, /* ID of user who changed constituent info */
+    netCommunityMember  BINARY(1)   NOT NULL, /* Net Community Member Status */
+    doNotMail       BINARY(1)       NOT NULL, /* Recieve mail notifications */
+    doNotEmail       BINARY(1)       NOT NULL, /* Recieve email notifications */
+    doNotPhone       BINARY(1)       NOT NULL, /* Recieve phone calls */
+    customIdentifier VARCHAR(100)   NULL,   /* Check current use */
+    addedBy         VARCHAR(100)    NOT NULL, /* ID of user who added constituent */
+    changedBy       VARCHAR(100)    NULL, /* ID of user who changed constituent info */
     /*Date Added */
     /*Date Changed */
     /*Time Stamp */
     /*Time Stamp Long */
-    isgroup         BINARY(1)       NOT NULL,
-    displayname     VARCHAR(100)    NOT NULL, /* Display Name */
+    isGroup         BINARY(1)       NOT NULL,
+    displayName     VARCHAR(100)    NOT NULL, /* Display Name */
     gender          VARCHAR(7)      NOT NULL, /* Gender */
     /* Title 2 Code ID */
     /* Suffix 2 Code ID */
-    lookupid        VARCHAR(100)    NOT NULL, /* Unknown use */
-    isconstituent   BINARY(1)       NOT NULL, /* ??????? Why would you need this */
+    lookupID        VARCHAR(100)    NOT NULL, /* Unknown use */
+    isConstituent   BINARY(1)       NOT NULL, /* ??????? Why would you need this */
     age             DECIMAL(3)      NOT NULL, /* Age */
     name            VARCHAR(154)    NOT NULL, /* Because you need five variables to hold name */ 
-    CONSTRAINT PK_Person PRIMARY KEY (constituentid)   
-);
+    CONSTRAINT CONSTITUENT_pkey PRIMARY KEY (constituentID) );
+
+/* Cooper 24/05/18 */
+CREATE TABLE APPUSER(
+    constituentID   VARCHAR(100)    NOT NULL, /* Universal Unique Identifier */ /* check storage method */
+    username        VARCHAR(20)     NOT NULL, /* Unique user name */
+    passHash        VARCHAR(32)     NOT NULL, /* Hash value of password */
+    salt            DECIMAL(3)      NOT NULL, /* Random salt value to reduce collision and false positive risk */
+    CONSTRAINT USER_pkey PRIMARY KEY (constituentID, username),
+    CONSTRAINT USER_fkey1 FOREIGN KEY (constituentID)
+        REFERENCES CONSTITUENT (constituentID) );
+
+/* Cooper 24/05/18 */
+CREATE TABLE EDUCATIONALINSTITUTION(
+    /*uniID*/   
+    addedBy         VARCHAR(100)    NOT NULL, /* ID of user who added constituent */
+    changedBy       VARCHAR(100)    NULL, /* ID of user who changed constituent info */
+    /*Date Added */
+    /*Date Changed */
+    /*Time Stamp */
+    /*Time Stamp Long */
+    ficeCode        NVARCHAR(50)    NULL, /* Unknown */
+    classificationCode TINYINT      NULL, /* Unknown */
+    name            NVARCHAR(100)   NOT NULL, /* Name of University */
+    isAffiliated    BINARY(1)       NOT NULL, /* Is the Institution Associated with UOW */
+    city            NVARCHAR(150)   NOT NULL, /* City the institution is based in */
+    /*stateID*/         
+    /*countryID*/
+    CONSTRAINT EDUCATIONALINSTITUTION_pkey PRIMARY KEY (name) );
---
+++ Database/dbdrop.sql
@@ -0,0 +1,11 @@
+/* Drop Database Tables */
+/*    Design Based on RE Database */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*    Last Edited: Cooper Brindle 24/05/2018 12:14pm*/
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+DROP TABLE APPUSER;
+DROP TABLE EDUCATIONALINSTITUTION;
+DROP TABLE CONSTITUENT;
