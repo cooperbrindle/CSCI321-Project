9cbaeab970a45577cfade4242b5483bdb2d620d6
csit321mf06a <csit321mf06a@proj-mac8.adeis.uow.edu.au>
2018-05-24 14:51:40 +1000

    sha

--- Components/sha-256.js
+++ Components/sha-256.js
@@ -12,7 +12,7 @@
  *
  * This is an annotated direct implementation of FIPS 180-4, without any optimisations. It is
  * intended to aid understanding of the algorithm rather than for production use.
- *
+ /*
  * While it could be used where performance is not critical, I would recommend using the ‘Web
  * Cryptography API’ (developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest) for the browser,
  * or the ‘crypto’ library (nodejs.org/api/crypto.html#crypto_class_hash) in Node.js.
