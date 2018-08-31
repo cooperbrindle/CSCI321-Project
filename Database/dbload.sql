30fabcc7a62f994a2c4741e11bd9ccddf5ac039f
Cooper Brindle <cjb889@uowmail.edu.au>
2018-08-31 17:32:07 +1000

    discounts db

--- Database/dbload.sql
+++ Database/dbload.sql
@@ -5,4 +5,21 @@
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-INSERT INTO DISCOUNTS VALUES ('')
+INSERT INTO DISCOUNTS VALUES ('unibar', 'uow', 'UNIBAR', 'dc', 'Purchase event tickets at student prices!');
+INSERT INTO DISCOUNTS VALUES ('studperf', 'uow', 'Student Performances', 'dc', 'Discounted tickets to all theatre productions from Australias rising stars!');
+INSERT INTO DISCOUNTS VALUES ('uniActive', 'uow', 'UNIACTIVE Wollongong Campus and Innovation Campus Gyms', 'dc', 'Concession rate on a variety of memberships, visit passes and casual visits!');
+INSERT INTO DISCOUNTS VALUES ('uniCentre', 'uow', 'UNICENTRE Market ALley', 'dc', 'Stall fees at student prices for this fortnightly open-air market!');
+INSERT INTO DISCOUNTS VALUES ('uowFC', 'uow', 'UOW Football Club', 'dc', 'Free admission to al UOW Football Club home games at Kooloobong Oval!');
+INSERT INTO DISCOUNTS VALUES ('rmb', 'local', 'RMB Lawyers', 'dc', '10% discount, except no win no fee work. Phone +61 2 4228 8288!');
+INSERT INTO DISCOUNTS VALUES ('livingroom', 'local', 'The Living Room', 'dc', '15% discount, 7 days a week!');
+INSERT INTO DISCOUNTS VALUES ('northbeach', 'local', 'Northbeach Pavilion', 'dc', '10% discount on dining for breakfast, lunch and dinner (not available at the kiosk).');
+INSERT INTO DISCOUNTS VALUES ('relish', 'local', 'Relish on Addison', 'dc', '10% discount, max group of 10 Tuesday to Friday!');
+INSERT INTO DISCOUNTS VALUES ('littleprince', 'local', 'The Little Prince', 'dc', 'Happy Hour prices until 9pm!');
+INSERT INTO DISCOUNTS VALUES ('wgolfclub', 'local', 'Wollongong Golf Club', 'dc', 'Round of gold (18 Holes) for $22. Bookings essential. 10% discount of best available rate of the day at The Best Western City Sand for those playing golf!');
+INSERT INTO DISCOUNTS VALUES ('skydive', 'national', 'Skydive Australia', 'dc', '$40 off your 14,000ft tandem skydive at anny of the 15 Skydive the Beach and Byond locations across Australia!');
+INSERT INTO DISCOUNTS VALUES ('dell', 'national', 'DELL Computers', 'em', 'Receive an exclusive discount on Dell products!');
+/* AVIS and Budget */
+/* Wine Direct */
+/* TFE Hotels */
+/* Hotel Storm */
+/* Experience OZ */
