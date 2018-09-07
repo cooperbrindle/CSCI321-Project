c94c72159c6adb7c42b5a5d79f8ea87de6f31410
Cooper Brindle <cjb889@uowmail.edu.au>
2018-09-07 14:54:04 +1000

    dbchanges

--- Backend/sql/dbload.sql
+++ Backend/sql/dbload.sql
@@ -6,20 +6,27 @@
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+/* Load Constituents */
+LOAD DATA LOCAL INFILE 'constituent.csv' 
+INTO TABLE CONSTITUENT 
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 /* Load Discounts */
-INSERT INTO DISCOUNTS VALUES ('unibar', 'uow', 'UNIBAR', 'dc', 'Purchase event tickets at student prices!');
-INSERT INTO DISCOUNTS VALUES ('studperf', 'uow', 'Student Performances', 'dc', 'Discounted tickets to all theatre productions from Australias rising stars!');
-INSERT INTO DISCOUNTS VALUES ('uniActive', 'uow', 'UNIACTIVE Wollongong Campus and Innovation Campus Gyms', 'dc', 'Concession rate on a variety of memberships, visit passes and casual visits!');
-INSERT INTO DISCOUNTS VALUES ('uniCentre', 'uow', 'UNICENTRE Market ALley', 'dc', 'Stall fees at student prices for this fortnightly open-air market!');
-INSERT INTO DISCOUNTS VALUES ('uowFC', 'uow', 'UOW Football Club', 'dc', 'Free admission to all UOW Football Club home games at Kooloobong Oval!');
+INSERT INTO DISCOUNTS VALUES ('unibar', 'uow', 'UNIBAR', 'dc', 'Purchase event tickets at student prices!', NULL);
+INSERT INTO DISCOUNTS VALUES ('studperf', 'uow', 'Student Performances', 'dc', 'Discounted tickets to all theatre productions from Australias rising stars!', NULL);
+INSERT INTO DISCOUNTS VALUES ('uniActive', 'uow', 'UNIACTIVE Wollongong Campus and Innovation Campus Gyms', 'dc', 'Concession rate on a variety of memberships, visit passes and casual visits!', NULL);
+INSERT INTO DISCOUNTS VALUES ('uniCentre', 'uow', 'UNICENTRE Market ALley', 'dc', 'Stall fees at student prices for this fortnightly open-air market!', NULL);
+INSERT INTO DISCOUNTS VALUES ('uowFC', 'uow', 'UOW Football Club', 'dc', 'Free admission to all UOW Football Club home games at Kooloobong Oval!', NULL);
 INSERT INTO DISCOUNTS VALUES ('rmb', 'local', 'RMB Lawyers', 'dc', '10% discount, except no win no fee work. Phone +61 2 4228 8288!', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow192897.jpg');
 INSERT INTO DISCOUNTS VALUES ('livingroom', 'local', 'The Living Room', 'dc', '15% discount, 7 days a week!', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow192916.png');
 INSERT INTO DISCOUNTS VALUES ('northbeach', 'local', 'Northbeach Pavilion', 'dc', '10% discount on dining for breakfast, lunch and dinner (not available at the kiosk).', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow192900.jpg');
 INSERT INTO DISCOUNTS VALUES ('relish', 'local', 'Relish on Addison', 'dc', '10% discount, max group of 10 Tuesday to Friday!', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow192901.jpg');
 INSERT INTO DISCOUNTS VALUES ('littleprince', 'local', 'The Little Prince', 'dc', 'Happy Hour prices until 9pm!', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow192902.jpg');
 INSERT INTO DISCOUNTS VALUES ('wgolfclub', 'local', 'Wollongong Golf Club', 'dc', 'Round of gold (18 Holes) for $22. Bookings essential. 10% discount of best available rate of the day at The Best Western City Sand for those playing golf!', 'https://www.uow.edu.au/content/groups/public/@web/@community/documents/mm/uow199360.jpg');
-INSERT INTO DISCOUNTS VALUES ('skydive', 'national', 'Skydive Australia', 'dc', '$40 off your 14,000ft tandem skydive at anny of the 15 Skydive the Beach and Byond locations across Australia!');
-INSERT INTO DISCOUNTS VALUES ('dell', 'national', 'DELL Computers', 'em', 'Receive an exclusive discount on Dell products!');
+INSERT INTO DISCOUNTS VALUES ('skydive', 'national', 'Skydive Australia', 'dc', '$40 off your 14,000ft tandem skydive at anny of the 15 Skydive the Beach and Byond locations across Australia!', NULL);
+INSERT INTO DISCOUNTS VALUES ('dell', 'national', 'DELL Computers', 'em', 'Receive an exclusive discount on Dell products!', NULL);
 /* AVIS and Budget */
 /* Wine Direct */
 /* TFE Hotels */
