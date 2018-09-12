0f0330d0ade91da3bdd15ea92fd724095422f518
csit321mf06a <csit321mf06a@proj-mac8.adeis.uow.edu.au>
2018-09-12 15:31:26 +1000

    index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use('/auth', require('./auth'));
 router.use('/promotions', require('./promotions'));
+router.use('/events', require('./events'));
 
 module.exports = router;
