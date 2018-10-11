eac1fcfcdc881aa11a99ddadc7f15a8e8768374c
Cooper Brindle <cjb889@uowmail.edu.au>
2018-10-11 19:57:59 +1100

    minor changes boi

--- export.sh
+++ export.sh
@@ -17,3 +17,8 @@ mv $DB_BACKUP/03 $DB_BACKUP/04
 mv $DB_BACKUP/02 $DB_BACKUP/03
 mv $DB_BACKUP/01 $DB_BACKUP/02
 mkdir $DB_BACKUP/01
+
+echo "* Creating new backup..."
+mysql --password=$DB_PASSWD -h 'localhost' -e "SELECT * FROM alumniapp.CONSTITUENTEXPORT WHERE datecreated >= DATE_SUB(NOW(), INTERVAL 1 DAY)" > $DB_BACKUP/01/exporteddata-`date +%Y-%m-%d`.txt
+echo "* File: " $DB_BACKUP/01/exporteddata-`date +%Y-%m-%d`.txt
+echo "-----
