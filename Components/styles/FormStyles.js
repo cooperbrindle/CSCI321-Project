c8a2b82a72a96572c783dba2d46e1c87e2cd7276
Cooper Brindle <cjb889@uowmail.edu.au>
2018-10-12 21:27:30 +1100

    dans dumb

--- Components/styles/FormStyles.js
+++ Components/styles/FormStyles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 
-export const formStyles = StyleSheet.create({
+export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0C2340',
