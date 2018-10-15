b18ca9e08eb21f2a795fb774324a93faac18c6f2
Shiny380 <daniel.mckinnell43@gmail.com>
2018-10-15 14:07:14 +1100

    fxed no username, changed sign up to username instead of email

--- Components/SignUp/SignUpFinish.js
+++ Components/SignUp/SignUpFinish.js
@@ -40,8 +40,9 @@ export default class SignUpFinish extends Component {
     submitForm() {
         
         this.setState({isLoading: true, errorMessage: ''});
-        
-        if(this.state.password != this.state.passwordConf || (this.state.password == '' || this.state.passwordConf == '')){
+        if(this.state.username == ''){
+            this.setState({isLoading: false, errorMessage: 'No username'});
+        }else if(this.state.password != this.state.passwordConf || (this.state.password == '' || this.state.passwordConf == '')){
             this.setState({isLoading: false, errorMessage: 'Passwords do not match'});
         }else{
             this.state.vultr.registerUser(this.state.username, this.state.password, this.state.id)
